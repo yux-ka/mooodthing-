b77bfd4ea35dc40b000c73b46d220864cb5a0f42
“yuky8500@gmail.com” <yuky8500@gmail.com>
2025-10-20 16:36:12 +0500

    'login'

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,11 @@
-import { LoginForm } from "@/components/login-form"
-
-export default function Page() {
+import React from 'react'
+import { LoginForm } from '@/components/login-form'
+const page = () => {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
+    <div>
+        <LoginForm/>
     </div>
   )
 }
+
+export default page

47b8e3cca5e0589ae8c7cc6b82d5d60e32146e9c
Zaul-A <zaulforloop902@gmail.com>
2025-10-22 15:59:19 +0500

    zaul tried to add something

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import { LoginForm } from '@/components/login-form'
-const page = () => {
-  return (
-    <div>
-        <LoginForm/>
-    </div>
-  )
-}
 
-export default page
+
+export default function signin() {
+    return(
+        <div>Login page..</div>
+    )
+}
